7679b945007b410034d78faf59bb8b6a88048b6e
myusername <UlisesOviedou170@gmail.com>
2025-11-26 07:53:00 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'https://backendfirst-h91z.onrender.com',
     googleClientId: '524581011070-bvs8bepgpac2ijms5acr2e1po776m1h9.apps.googleusercontent.com'
   };
   
